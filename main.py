022c8c403308febeb8d85e3b9d77cfb419abae41
yangzhr1 <87291965+yangzhr1@users.noreply.github.com>
2025-07-09 20:49:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,27 @@ import argparse
 import torch
 from pre_train import pretrain
 from model.GraphLoRA import transfer
+from util import get_parameter
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pretrain_dataset', type=str, default='PubMed')
     parser.add_argument('--test_dataset', type=str, default='CiteSeer')
-    parser.add_argument('--gpu_id', type=int, default=0)
+    parser.add_argument('--gpu_id', type=int, default=1)
     parser.add_argument('--pretext', type=str, default='GRACE')
     parser.add_argument('--config', type=str, default='./config.yaml')
+    parser.add_argument('--para_config', type=str, default='./config2.yaml')
     parser.add_argument('--is_pretrain', type=bool, default=False)
     parser.add_argument('--is_transfer', type=bool, default=True)
     parser.add_argument('--is_reduction', type=bool, default=True)
-    parser.add_argument('--few', type=bool, default=True)
+    parser.add_argument('--few', type=bool, default=False)
     parser.add_argument('--shot', type=int, default=10)
     parser.add_argument('--tau', type=float, default=0.5)
-    parser.add_argument('--l1', type=float, default=0.5)
-    parser.add_argument('--l2', type=float, default=1)
-    parser.add_argument('--l3', type=float, default=2)
-    parser.add_argument('--l4', type=float, default=10)
-    parser.add_argument('--lr1', type=float, default=1e-2)
-    parser.add_argument('--lr2', type=float, default=1e-2)
-    parser.add_argument('--lr3', type=float, default=1e-5)
-    parser.add_argument('--wd1', type=float, default=1e-2)
-    parser.add_argument('--wd2', type=float, default=2e-3)
-    parser.add_argument('--wd3', type=float, default=1e-1)
     parser.add_argument('--sup_weight', type=float, default=0.2)
     parser.add_argument('--r', type=int, default=32)
-    parser.add_argument('--num_epochs', type=int, default=200)
     args = parser.parse_args()
+    args = get_parameter(args)
 
     assert args.gpu_id in range(0, 2)
     torch.cuda.set_device(args.gpu_id)
